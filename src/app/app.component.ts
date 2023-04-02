430e9552ae24ebc05dfdf829d3dd2aecd02d8c6f
kkepinska <katarzyna.kpinska@student.uj.edu.pl>
2023-04-02 11:48:11 +0200

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io, Socket } from "socket.io-client";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Hanabi';
+
+  joinRoom() : void{
+    const socket = io('http://localhost:8080');
+    socket.emit("join", "hello :)", (response:Response) => {
+      console.log(response); // "got it"
+    });
+    console.log("clicked!\n")
+  }
+
 }
